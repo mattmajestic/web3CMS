13b1bad63574949830ca37098472fd9e817ced65
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-08-01 01:59:55 -0400

    Update invoice.py

--- pages/invoice.py
+++ pages/invoice.py
@@ -5,8 +5,8 @@ import pandas as pd
 import streamlit as st
 from streamlit.components.v1 import iframe
 
-st.set_page_config(layout="centered", page_icon="🐪", page_title=" litCRM Invoice Generator")
-st.title("❄ litCRM Invoice Generator")
+st.set_page_config(layout="centered", page_icon="🐪", page_title=" litCRM")
+st.title("❄ litCRM (Streamlit Based CRM)")
 
 left, right = st.columns(2)
 
@@ -14,14 +14,14 @@ env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape())
 template = env.get_template("template.html")
 
 left.write("Update the Invoice Template Below:")
+products = pd.read_csv("./data/products.csv")
 form = left.form("template_form")
-service = form.text_input("Invoice Service","Analytics Consulting")
+service = form.text_input("Invoice Service",products['Name'])
 client = form.selectbox(
     "Client",
     ["CNN", "Penn State","Coca Cola Florida LLC","McAfee"],
     index=0,
 )
-products = pd.read_csv("./data/products.csv")
 rate = form.number_input("Hourly Rate", 1, 10000, 100,100)
 submit = form.form_submit_button("Generate Invoice")
 st.dataframe(products)
