3f05233b2cbb635a9c2267fbe17b2a729f58532f
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-08-02 08:06:30 -0400

    Update invoice.py

--- pages/invoice.py
+++ pages/invoice.py
@@ -5,7 +5,6 @@ import datetime
 import pandas as pd
 import streamlit as st
 from streamlit.components.v1 import iframe
-import pycoingecko
 
 st.set_page_config(layout="centered", page_icon="🐪", page_title=" litCRM")
 st.title("❄ litCRM (Streamlit Based CRM)")
