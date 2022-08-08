b4c05de0fb3f1e1696ed43046a72dfefdd77bdfd
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-08-08 14:36:54 -0400

    Update invoice.py

--- pages/invoice.py
+++ pages/invoice.py
@@ -10,42 +10,7 @@ import yfinance as yf
 
 def home_page():
     st.markdown("# Home page 🎈")
-    st.markdown("# litCRM 🚀
-a lightweight crm built in Streamlit
-
-## Business Opportunity 💸
-  -  Large CRM Providers require expensive & long term contracts leaving market share for flexible + nimble solutions 💰
-  - Streamlit recently bought for $800m by Snowflake ❄️
-  - Enable crypto/defi functions within CRM 🎬
-
-## SDLC LifeCycle: Dev 🚝 
-
-## Tech Stack 📡
-- R Based in Shiny @ https://mmajestic.shinyapps.io/shinyCRM
-- Python Based in Streamlit @ https://mattmajestic-litcrm-pagesinvoice-mh000s.streamlitapp.com/
-- Streamlit Hosting
-- Docker/Container Enabled on ☁️ Cloud Providers including:
-  - Dockerhub
-  - AWS ECR
-  - Azure ACR
-  - Google Cloud Container Registry
-
-## Install litCRM Boilerplate 💥
-```bash
-git clone https://github.com/mattmajestic/litCRM.git
-```
-
-## Update the csvs in the data/ directory 📂
-- Contacts
-- Products
-- Opportunities
-
-## Product User Story 📕
-```mermaid
-flowchart LR
-    a[(Update Tables)] --> b([Upload to Github])
-    b --> c((Deploy to Streamlit Cloud))
- ```")
+    components.iframe("https://github.com/mattmajestic/litCRM/blob/main/README.md",640,480)
     st.text("Checkout the Shiny Demo")
     components.iframe("https://drive.google.com/file/d/1bpHOLX8RkjMzXAj5LtHyMrsmpZ06Gipg/preview",640,480)
     st.sidebar.markdown("# Home page 🎈")
