b2ac96da6a9f865e73cafe39750ba1ee5b5c5b42
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-08-10 20:31:53 -0400

    Update invoice.py

--- pages/invoice.py
+++ pages/invoice.py
@@ -25,7 +25,7 @@ def invoice():
     st.title("❄ litCRM (Streamlit Based CRM)")
     st.sidebar.markdown("Crypto Invoicing")
 
-    left, right = st.columns(4)
+    left, right = [3, 3]
 
     env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape())
     template = env.get_template("template.html")
