2f544a7039ee2fdff32b1808b014537bb78ed630
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-08-10 21:31:52 -0400

    Update invoice.py

--- pages/invoice.py
+++ pages/invoice.py
@@ -36,7 +36,7 @@ def invoice():
     service_choices = products[["Name"]]
     form = left.form("template_form")
     service = form.selectbox("Invoice Service",service_choices)
-    coin = right.selectbox("Invoice Currency",["ETH","BTC","USDC","USD (Cash)"],index=0)
+    coin = form.selectbox("Invoice Currency",["ETH","BTC","USDC","USD (Cash)"],index=0)
     client = form.selectbox("Client",["CNN", "Penn State","Coca Cola Florida LLC","McAfee"],index=0)
     start_period = form.date_input("Start of Invoice Time Period", start)
     hours = form.number_input("Hours", 1, 80, 40)
