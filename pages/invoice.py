e7c3e6a90af6d30767ae397ef4d2a80747ff3d73
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-08-12 17:01:56 -0400

    Update invoice.py

--- pages/invoice.py
+++ pages/invoice.py
@@ -7,6 +7,7 @@ from streamlit.components.v1 import iframe
 import streamlit.components.v1 as components
 import requests
 import yfinance as yf
+from pycoingecko import CoinGeckoAPI
 import web3
 from web3 import Web3, HTTPProvider 
 import json
@@ -153,83 +154,9 @@ def backend():
     tab4.json(jsonRaw)
      
 def dash():
-     st.sidebar.image(
-                         "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/z3ahdkytzwi1jxlpazje",
-                         width=50,
-     )
-
-     c1, c2 = st.columns([1, 8])
-
-     with c1:
-                         st.image(
-                                             "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/chart-increasing_1f4c8.png",
-                                             width=90,
-                         )
-
-     st.markdown(
-                         """# **Crypto Dashboard**
-     A simple cryptocurrency price app pulling price data from the [Binance API](https://www.binance.com/en/support/faq/360002502072), from [Data Professor](https://twitter.com/thedataprof).
-     """
-     )
-     st.header("**Selected Price**")
-     df = pd.read_json("https://api.binance.com/api/v3/ticker/24hr")
-     def round_value(input_value):
-                         if input_value.values > 1:
-                                             a = float(round(input_value, 2))
-                         else:
-                                             a = float(round(input_value, 8))
-                         return a
-     crpytoList = {
-                         "Price 1": "BTCBUSD",
-                         "Price 2": "ETHBUSD",
-                         "Price 3": "BNBBUSD",
-                         "Price 4": "XRPBUSD",
-                         "Price 5": "ADABUSD",
-                         "Price 6": "DOGEBUSD",
-                         "Price 7": "SHIBBUSD",
-                         "Price 8": "DOTBUSD",
-                         "Price 9": "MATICBUSD",
-     }
-     col1, col2, col3 = st.columns(3)
-     for i in range(len(crpytoList.keys())):
-                         selected_crypto_label = list(crpytoList.keys())[i]
-                         selected_crypto_index = list(df.symbol).index(crpytoList[selected_crypto_label])
-                         selected_crypto = st.sidebar.selectbox(
-                                             selected_crypto_label, df.symbol, selected_crypto_index, key=str(i)
-                         )
-                         col_df = df[df.symbol == selected_crypto]
-                         col_price = round_value(col_df.weightedAvgPrice)
-                         col_percent = f"{float(col_df.priceChangePercent)}%"
-                         if i < 3:
-                                             with col1:
-                                                                 st.metric(selected_crypto, col_price, col_percent)
-                         if 2 < i < 6:
-                                             with col2:
-                                                                 st.metric(selected_crypto, col_price, col_percent)
-                         if i > 5:
-                                             with col3:
-                                                                 st.metric(selected_crypto, col_price, col_percent)
-
-     @st.cache
-     def convert_df(df):
-                         # IMPORTANT: Cache the conversion to prevent computation on every rerun
-                         return df.to_csv().encode("utf-8")
-     csv = convert_df(df)
-     st.download_button(
-                         label="Download data as CSV",
-                         data=csv,
-                         file_name="large_df.csv",
-                         mime="text/csv",
-     )
-     st.dataframe(df, height=2000)
-     st.markdown(
-                         """
-     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-     """,
-                         unsafe_allow_html=True,
-     )
+    cg = CoinGeckoAPI() 
+    btc = cg.get_price(ids='bitcoin', vs_currencies='usd', include_market_cap='true', include_24hr_vol='true', include_24hr_change='true', include_last_updated_at='true')
+    st.json(btc)
 
 page_names_to_funcs = {
     "Home Page": home_page,
@@ -237,6 +164,7 @@ page_names_to_funcs = {
     "Clients": clients,
     "Products": products,
     "Opportunties": opportunities,
+    "Coin Gecko API": dash,
     "Backend": backend,
     
 }
