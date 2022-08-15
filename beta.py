9eb16fa7245de6564b7f146a8204a70e13b80391
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-08-15 19:24:00 -0400

    Update beta.py

--- beta.py
+++ beta.py
@@ -53,12 +53,9 @@ def home_page():
          </html>
          <script>
          async function connect() {
-           ethereum.request({ method: "eth_requestAccounts" });
-           window.web3 = new Web3(window.ethereum);
-           const account = web3.eth.accounts;
-           const walletAddress = account.givenProvider.selectedAddress;
-           console.log(`Wallet: ${walletAddress}`);
-         }
+          accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+          console.log(accounts)
+          };
          </script>
          """
          my_html = f"<script>{my_js}</script>"
