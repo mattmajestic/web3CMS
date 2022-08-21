8bc73f72ca01236f0083032c92e8a4172b900c8f
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-08-21 18:34:03 -0400

    Update beta.py

--- beta.py
+++ beta.py
@@ -24,11 +24,11 @@ st.set_page_config(
  )
 
 def home_page():
+    st.title("❄ web3BMS (Web3 Enabled Business Management Solution)")
     left, right = st.columns([7,5])
     cg_html = '''
     <script src="https://widgets.coingecko.com/coingecko-coin-price-marquee-widget.js"></script><coingecko-coin-price-marquee-widget  coin-ids="bitcoin,ethereum,eos,ripple,litecoin" currency="usd" background-color="#ffffff" locale="en"></coingecko-coin-price-marquee-widget>
     '''
-    st.title("❄ web3BMS (Web3 Enabled Business Management Solution)")
     left.text("Web3 enabled Business Management Software 🚀")
     left.write("Invoicing in cash & crypto per invoice")
     right.text("Web3 Code Management ⌚")
