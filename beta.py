5b2faefeff2e5d852e76a377e3a564f175c69186
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-07-03 20:23:15 +0000

    update

--- beta.py
+++ beta.py
@@ -57,7 +57,7 @@ def invoice():
     rate = form.number_input("Hourly Rate", 1, 10000, 120,120)
     notes = form.text_input("Add Any Additional Notes")
     submit = form.form_submit_button("Generate Invoice")
-    coin_addy = right.selectbox("Invoice Currency Price",["0x2170Ed0880ac9A755fd29B2688956BD959F933F8", "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c","0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d"],index=0)
+    coin_addy = right.selectbox("Invoice Send Address",["0x2170Ed0880ac9A755fd29B2688956BD959F933F8", "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c","0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d"],index=0)
     apiURL = "https://api.pancakeswap.info/api/v2/tokens/"
     response = requests.get(url = apiURL + coin_addy)
     jsonRaw = response.json()
