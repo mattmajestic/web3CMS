d94bcd59c25b61175135e68381e8d605334eb089
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-07-04 07:45:50 -0400

    update

--- beta.py
+++ beta.py
@@ -13,7 +13,7 @@ from pycoingecko import CoinGeckoAPI
 from web3 import Web3, HTTPProvider 
 import json
 import time
-from lunarcrush import LunarCrush
+# from lunarcrush import LunarCrush
 
 # Read the contents of the README.md file
 with open('README.md', 'r') as file:
@@ -26,17 +26,6 @@ with open("index.html", "r") as file:
 # Load and render the main.js file
     with open("metamask/main.js", "r") as js_file:
         js_code = js_file.read()
-
-def connect_metamask():
-    if "ethereum" not in window:
-        st.warning("Metamask not detected. Please make sure you have Metamask installed and running.")
-        return
-
-    try:
-        window.ethereum.enable()
-        st.success("Metamask connection successful!")
-    except Exception as e:
-        st.error(f"Metamask connection failed: {str(e)}")
 
 st.set_page_config(
      page_title="litBMS",
@@ -93,9 +82,45 @@ def invoice():
     right.write(invoice_total)
     with right:
          components.html(cg_html)
-         st.title("Metamask Connection")
-         if st.button("Connect Metamask"):
-            st.success("Metamask connection popup")
+         st.write("Connect Wallet")
+         if st.button("Connect to Metamask"):
+            # Write the JavaScript code within st.markdown
+            st.markdown(
+                """
+                <script>
+                    // JavaScript code to connect Metamask
+                    const connectMetamask = () => {
+                        if (typeof window.ethereum !== 'undefined') {
+                            startLoading();
+
+                            ethereum
+                                .request({ method: 'eth_requestAccounts' })
+                                .then((accounts) => {
+                                    const account = accounts[0];
+                                    walletID.innerHTML = `Wallet connected: <span>${account}</span>`;
+                                    stopLoading();
+                                })
+                                .catch((error) => {
+                                    console.log(error, error.code);
+                                    alert(error.code);
+                                    stopLoading();
+                                });
+                        } else {
+                            if (isMobile()) {
+                                mobileDeviceWarning.classList.add('show');
+                            } else {
+                                window.open('https://metamask.io/download/', '_blank');
+                                installAlert.classList.add('show');
+                            }
+                        }
+                    };
+
+                    // Call the function to connect Metamask when the button is clicked
+                    document.getElementById('connectButton').addEventListener('click', connectMetamask);
+                </script>
+                """,
+                unsafe_allow_html=True
+            )
          
     if submit:
         html = template.render(
@@ -122,51 +147,51 @@ def ai_chat():
         st.write(f"User has sent the following prompt: {prompt}")
 
 
-def clients():
-    st.snow()
-    left,right = st.columns([5,5])
-    uploaded_file = left.file_uploader("Upload your Clients", type=['csv','xlsx'],accept_multiple_files=False,key="fileUploader")
-    st.sidebar.markdown("# Client Management")
-    if uploaded_file is not None:
-        contacts = pd.read_csv(uploaded_file)
-    else:
-        contacts = pd.read_csv("./data/contacts.csv")
-    right.dataframe(contacts)
-    my_bar = st.progress(0)
-    for percent_complete in range(100):
-         time.sleep(0.05)
-         my_bar.progress(percent_complete + 1)
-    my_bar.empty()
+# def clients():
+#     st.snow()
+#     left,right = st.columns([5,5])
+#     uploaded_file = left.file_uploader("Upload your Clients", type=['csv','xlsx'],accept_multiple_files=False,key="fileUploader")
+#     st.sidebar.markdown("# Client Management")
+#     if uploaded_file is not None:
+#         contacts = pd.read_csv(uploaded_file)
+#     else:
+#         contacts = pd.read_csv("./data/contacts.csv")
+#     right.dataframe(contacts)
+#     my_bar = st.progress(0)
+#     for percent_complete in range(100):
+#          time.sleep(0.05)
+#          my_bar.progress(percent_complete + 1)
+#     my_bar.empty()
 
-def products():
-    left, right = st.columns([5,5])
-    uploaded_file = left.file_uploader("Upload your Products", type=['csv','xlsx'],accept_multiple_files=False,key="fileUploader")
-    st.sidebar.markdown("# Product Management")
-    if uploaded_file is not None:
-        products = pd.read_csv(uploaded_file)
-    else:
-        products = pd.read_csv("./data/products.csv")
-    right.dataframe(products)
-    my_bar = st.progress(0)
-    for percent_complete in range(100):
-         time.sleep(0.05)
-         my_bar.progress(percent_complete + 1)
-    my_bar.empty()
+# def products():
+#     left, right = st.columns([5,5])
+#     uploaded_file = left.file_uploader("Upload your Products", type=['csv','xlsx'],accept_multiple_files=False,key="fileUploader")
+#     st.sidebar.markdown("# Product Management")
+#     if uploaded_file is not None:
+#         products = pd.read_csv(uploaded_file)
+#     else:
+#         products = pd.read_csv("./data/products.csv")
+#     right.dataframe(products)
+#     my_bar = st.progress(0)
+#     for percent_complete in range(100):
+#          time.sleep(0.05)
+#          my_bar.progress(percent_complete + 1)
+#     my_bar.empty()
 
-def opportunities():
-    left,right = st.columns([5,5])
-    uploaded_file = left.file_uploader("Upload your Opportunities", type=['csv','xlsx'],accept_multiple_files=False,key="fileUploader")
-    st.sidebar.markdown("# Opportunities Tracker")
-    if uploaded_file is not None:
-        opportunities = pd.read_csv(uploaded_file)
-    else:
-        opportunities = pd.read_csv("./data/opportunities.csv")
-    right.dataframe(opportunities)
-    my_bar = st.progress(0)
-    for percent_complete in range(100):
-         time.sleep(0.05)
-         my_bar.progress(percent_complete + 1)
-    my_bar.empty()
+# def opportunities():
+#     left,right = st.columns([5,5])
+#     uploaded_file = left.file_uploader("Upload your Opportunities", type=['csv','xlsx'],accept_multiple_files=False,key="fileUploader")
+#     st.sidebar.markdown("# Opportunities Tracker")
+#     if uploaded_file is not None:
+#         opportunities = pd.read_csv(uploaded_file)
+#     else:
+#         opportunities = pd.read_csv("./data/opportunities.csv")
+#     right.dataframe(opportunities)
+#     my_bar = st.progress(0)
+#     for percent_complete in range(100):
+#          time.sleep(0.05)
+#          my_bar.progress(percent_complete + 1)
+#     my_bar.empty()
 
 def backend():
     products = pd.read_csv("./data/products.csv")
@@ -187,11 +212,6 @@ def backend():
     tab3.data_editor(opportunities)
     tab3.button("Save Ops Table")
     tab4.text("Coin Currency History")
-    #coin_addy = tab4.selectbox("Coin Adrress",["0x2170Ed0880ac9A755fd29B2688956BD959F933F8", "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c"],index=0)
-    # apiURL = "https://api.pancakeswap.info/api/v2/tokens/"
-    # response = requests.get(url = apiURL + coin_addy)
-    # jsonRaw = response.json()
-    # tab4.json(jsonRaw)
     cg = CoinGeckoAPI()
     cg_category = cg.get_coins_categories()
     jsonRaw = json.dumps(cg_category)
@@ -199,28 +219,28 @@ def backend():
     tab4.dataframe(df)
     
      
-def api():
-    left, center, right = st.columns([4,4,4])
-    cg = CoinGeckoAPI() 
-    btc = cg.get_price(ids='bitcoin', vs_currencies='usd', include_market_cap='true', include_24hr_vol='true', include_24hr_change='true', include_last_updated_at='true')
-    with left.container():
-        left.title("Coin Gecko API")
-        left.json(btc)
-        left.image("https://static.coingecko.com/s/coingecko-mascot-suit-b1a9df2b041094a017948f1d184f1aa263e779d4e1f22c437e835b74f0b00073.png",width=200)
-    with center.container():
-        center.title("PancakeSwap API")
-        apiURL = "https://api.pancakeswap.info/api/v2/tokens/"
-        coin_addy = center.selectbox("Invoice Currency Price",["0x2170Ed0880ac9A755fd29B2688956BD959F933F8", "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c","0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d"],index=0)
-        response = requests.get(url = apiURL + coin_addy)
-        pancake = response.json()
-        center.json(pancake)
-        center.image("https://www.pngall.com/wp-content/uploads/10/PancakeSwap-Crypto-Logo-PNG-Images.png",width=200)
-    lc = LunarCrush()
-    eth_1_year_data = lc.get_assets(symbol=['ETH'],data_points=365, interval='day')
-    with right.container():
-        right.title("LunarCrush API")
-        right.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwRhSbOtWFqO4IjI0vcb_gNsTK644-bdO3t_YkW_qq&s",width=200)
-        right.write(eth_1_year_data)
+# def api():
+#     left, center, right = st.columns([4,4,4])
+#     cg = CoinGeckoAPI() 
+#     btc = cg.get_price(ids='bitcoin', vs_currencies='usd', include_market_cap='true', include_24hr_vol='true', include_24hr_change='true', include_last_updated_at='true')
+#     with left.container():
+#         left.title("Coin Gecko API")
+#         left.json(btc)
+#         left.image("https://static.coingecko.com/s/coingecko-mascot-suit-b1a9df2b041094a017948f1d184f1aa263e779d4e1f22c437e835b74f0b00073.png",width=200)
+#     with center.container():
+#         center.title("PancakeSwap API")
+#         apiURL = "https://api.pancakeswap.info/api/v2/tokens/"
+#         coin_addy = center.selectbox("Invoice Currency Price",["0x2170Ed0880ac9A755fd29B2688956BD959F933F8", "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c","0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d"],index=0)
+#         response = requests.get(url = apiURL + coin_addy)
+#         pancake = response.json()
+#         center.json(pancake)
+#         center.image("https://www.pngall.com/wp-content/uploads/10/PancakeSwap-Crypto-Logo-PNG-Images.png",width=200)
+#     lc = LunarCrush()
+#     eth_1_year_data = lc.get_assets(symbol=['ETH'],data_points=365, interval='day')
+#     with right.container():
+#         right.title("LunarCrush API")
+#         right.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwRhSbOtWFqO4IjI0vcb_gNsTK644-bdO3t_YkW_qq&s",width=200)
+#         right.write(eth_1_year_data)
 
 page_names_to_funcs = {
     "About ✏️": home_page,
