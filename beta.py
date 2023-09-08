1b90ec038307e6379bd7dbfc3033f8908745c210
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-09-08 18:53:06 -0400

    add button

--- beta.py
+++ beta.py
@@ -137,13 +137,9 @@ def invoice():
     start_period = form.date_input("Start of Invoice Time Period", start)
     hours = form.number_input("Hours", 1, 80, 40)
     rate = form.number_input("Hourly Rate", 1, 10000, 120, 120)
-    currency_type = form.checkbox("Select Currency Type", value=False,key="currency_type")
-    if currency_type:
-        crypto_type = form.selectbox("Select Cryptocurrency Type", ["BTC", "ETH", "USDC"], index=0,key="crypto_type")
-        if crypto_type:
-            form.write(f"Selected cryptocurrency: {crypto_type}")
-        else:
-            form.write("USD is selected")
+    show_crypto_button = form.button("Show Cryptocurrency Type")
+    if show_crypto_button:
+        crypto_type = form.selectbox("Select Cryptocurrency Type", ["BTC", "ETH", "USDC"], index=0)
     notes = form.text_input("Add Any Additional Notes")
     submit = form.form_submit_button("Generate Invoice")
     coin_addy = right.selectbox("Invoice Send Address",
