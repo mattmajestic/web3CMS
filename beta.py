a687fc346ba692d26e9370b1ae8e3e270cd49656
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-09-08 19:08:40 -0400

    dev docs

--- beta.py
+++ beta.py
@@ -1,6 +1,5 @@
 import streamlit as st
-from streamlit.components.v1 import iframe
-from streamlit.components.v1 import html
+from streamlit.components.v1 import iframe, html
 import streamlit.components.v1 as components
 from streamlit_javascript import st_javascript
 from wallet_connect import wallet_connect
@@ -14,12 +13,12 @@ from pycoingecko import CoinGeckoAPI
 from web3 import Web3, HTTPProvider 
 import json
 import time
-# from lunarcrush import LunarCrush
 import supabase
 import os
 import folium
 from geopy.geocoders import Nominatim
 import plotly.express as px
+from streamlit import session_state
 
 # Set your Supabase credentials as environment variables
 SUPABASE_URL = os.getenv("SUPABASE_URL")
@@ -40,47 +39,6 @@ with open("index.html", "r") as file:
     with open("metamask/main.js", "r") as js_file:
         js_code = js_file.read()
 
-# # Define the JavaScript code to connect Metamask
-# metamask_js = """
-#     <script>
-#         // JavaScript code to connect Metamask
-#         const connectMetamask = () => {
-#             if (typeof window.ethereum !== 'undefined') {
-#                 startLoading();
-
-#                 ethereum
-#                     .request({ method: 'eth_requestAccounts' })
-#                     .then((accounts) => {
-#                         const account = accounts[0];
-#                         walletID.innerHTML = `Wallet connected: <span>${account}</span>`;
-#                         stopLoading();
-#                     })
-#                     .catch((error) => {
-#                         console.log(error, error.code);
-#                         alert(error.code);
-#                         stopLoading();
-#                     });
-#             } else {
-#                 if (isMobile()) {
-#                     mobileDeviceWarning.classList.add('show');
-#                 } else {
-#                     window.open('https://metamask.io/download/', '_blank');
-#                     installAlert.classList.add('show');
-#                 }
-#             }
-#         };
-#     </script>
-# """
-
-# clerk_js = st_javascript("""
-#   async
-#   crossorigin="anonymous"
-#   data-clerk-publishable-key="pk_test_Zmxvd2luZy1oYWdmaXNoLTUuY2xlcmsuYWNjb3VudHMuZGV2JA"
-#   onload="window.Clerk.load()"
-#   src="https://flowing-hagfish-5.clerk.accounts.dev/npm/@clerk/clerk-js@4/dist/clerk.browser.js"
-#   type="text/javascript"
-# """)
-
 
 st.set_page_config(
      page_title="web3BMS",
@@ -100,18 +58,12 @@ def home_page():
 #     st.markdown(clerk_js, unsafe_allow_html=True)
 
 def invoice():
-    # Replace with supabase fetch
     products_db = supabase_client.table('products').select("*").execute()
     products_df = pd.DataFrame(products_db.data)
-    # products = pd.read_csv("./data/products.csv")
-    # Replace with supabase fetch
     contacts_db = supabase_client.table('contacts').select("*").execute()
     contacts_df = pd.DataFrame(contacts_db.data)
-    # contacts = pd.read_csv("./data/contacts.csv")
-    # Replace with supabase fetch
     opportunities_db = supabase_client.table('opportunities').select("*").execute()
     opportunities_df = pd.DataFrame(opportunities_db.data)
-    # opportunities = pd.read_csv("./data/opportunities.csv")
     
     cg_html = '''
     <script src="https://widgets.coingecko.com/coingecko-coin-list-widget.js"></script><coingecko-coin-list-widget  coin-ids="bitcoin,ethereum" currency="usd" locale="en"></coingecko-coin-list-widget>
@@ -137,7 +89,8 @@ def invoice():
     start_period = form.date_input("Start of Invoice Time Period", start)
     hours = form.number_input("Hours", 1, 80, 40)
     rate = form.number_input("Hourly Rate", 1, 10000, 120, 120)
-    show_crypto_button = form.button("Show Cryptocurrency Type")
+    if st.button("Show Cryptocurrency Type"):
+        show_crypto_button = True
     if show_crypto_button:
         crypto_type = form.selectbox("Select Cryptocurrency Type", ["BTC", "ETH", "USDC"], index=0)
     notes = form.text_input("Add Any Additional Notes")
@@ -251,65 +204,14 @@ def meetings():
             st.error("Please enter valid values for meeting count and annual salary.")
 
 
-# def clients():
-#     st.snow()
-#     left,right = st.columns([5,5])
-#     uploaded_file = left.file_uploader("Upload your Clients", type=['csv','xlsx'],accept_multiple_files=False,key="fileUploader")
-#     st.sidebar.markdown("# Client Management")
-#     if uploaded_file is not None:
-#         contacts = pd.read_csv(uploaded_file)
-#     else:
-#         contacts = pd.read_csv("./data/contacts.csv")
-#     right.dataframe(contacts)
-#     my_bar = st.progress(0)
-#     for percent_complete in range(100):
-#          time.sleep(0.05)
-#          my_bar.progress(percent_complete + 1)
-#     my_bar.empty()
-
-# def products():
-#     left, right = st.columns([5,5])
-#     uploaded_file = left.file_uploader("Upload your Products", type=['csv','xlsx'],accept_multiple_files=False,key="fileUploader")
-#     st.sidebar.markdown("# Product Management")
-#     if uploaded_file is not None:
-#         products = pd.read_csv(uploaded_file)
-#     else:
-#         products = pd.read_csv("./data/products.csv")
-#     right.dataframe(products)
-#     my_bar = st.progress(0)
-#     for percent_complete in range(100):
-#          time.sleep(0.05)
-#          my_bar.progress(percent_complete + 1)
-#     my_bar.empty()
-
-# def opportunities():
-#     left,right = st.columns([5,5])
-#     uploaded_file = left.file_uploader("Upload your Opportunities", type=['csv','xlsx'],accept_multiple_files=False,key="fileUploader")
-#     st.sidebar.markdown("# Opportunities Tracker")
-#     if uploaded_file is not None:
-#         opportunities = pd.read_csv(uploaded_file)
-#     else:
-#         opportunities = pd.read_csv("./data/opportunities.csv")
-#     right.dataframe(opportunities)
-#     my_bar = st.progress(0)
-#     for percent_complete in range(100):
-#          time.sleep(0.05)
-#          my_bar.progress(percent_complete + 1)
-#     my_bar.empty()
-
 def backend():
     # Replace with supabase fetch
     products_db = supabase_client.table('products').select("*").execute()
     products_df = pd.DataFrame(products_db.data)
-    # products = pd.read_csv("./data/products.csv")
-    # Replace with supabase fetch
     contacts_db = supabase_client.table('contacts').select("*").execute()
     contacts_df = pd.DataFrame(contacts_db.data)
-    # contacts = pd.read_csv("./data/contacts.csv")
-    # Replace with supabase fetch
     opportunities_db = supabase_client.table('opportunities').select("*").execute()
     opportunities_df = pd.DataFrame(opportunities_db.data)
-    # opportunities = pd.read_csv("./data/opportunities.csv")
     # Use geopy to get the coordinates for the center of the data points
     # geolocator = Nominatim(user_agent="myGeocoder")
     # location = geolocator.geocode(f"{contacts_df['City'].iloc[0]}, {contacts_df['State'].iloc[0]}")
@@ -369,38 +271,100 @@ def backend():
     tab4.dataframe(df)
     
      
-# def api():
-#     left, center, right = st.columns([4,4,4])
-#     cg = CoinGeckoAPI() 
-#     btc = cg.get_price(ids='bitcoin', vs_currencies='usd', include_market_cap='true', include_24hr_vol='true', include_24hr_change='true', include_last_updated_at='true')
-#     with left.container():
-#         left.title("Coin Gecko API")
-#         left.json(btc)
-#         left.image("https://static.coingecko.com/s/coingecko-mascot-suit-b1a9df2b041094a017948f1d184f1aa263e779d4e1f22c437e835b74f0b00073.png",width=200)
-#     with center.container():
-#         center.title("PancakeSwap API")
-#         apiURL = "https://api.pancakeswap.info/api/v2/tokens/"
-#         coin_addy = center.selectbox("Invoice Currency Price",["0x2170Ed0880ac9A755fd29B2688956BD959F933F8", "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c","0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d"],index=0)
-#         response = requests.get(url = apiURL + coin_addy)
-#         pancake = response.json()
-#         center.json(pancake)
-#         center.image("https://www.pngall.com/wp-content/uploads/10/PancakeSwap-Crypto-Logo-PNG-Images.png",width=200)
-#     lc = LunarCrush()
-#     eth_1_year_data = lc.get_assets(symbol=['ETH'],data_points=365, interval='day')
-#     with right.container():
-#         right.title("LunarCrush API")
-#         right.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwRhSbOtWFqO4IjI0vcb_gNsTK644-bdO3t_YkW_qq&s",width=200)
-#         right.write(eth_1_year_data)
+def api_docs():
+    st.title("web3bms API Documentation 🚝")
+
+    col1, col2 = st.columns([2, 3])
+
+    with col1:
+        st.header("API Usage")
+        st.write("""
+        To interact with the web3bms API, you can make HTTP requests to the following endpoints:
+
+        - **Endpoint 1:** `/api/endpoint1`
+          - Description: Description of the first API endpoint.
+          - Method: GET
+          - Parameters: 
+            - `param1`: Description of parameter 1.
+
+        - **Endpoint 2:** `/api/endpoint2`
+          - Description: Description of the second API endpoint.
+          - Method: POST
+          - Parameters: 
+            - `param2`: Description of parameter 2.
+        """)
+
+        st.header("CLI Usage")
+        st.write("""
+        The web3bms CLI provides command-line access to various features. Here are some common CLI commands:
+
+        - **Command 1:** `web3bms command1`
+          - Description: Description of command 1.
+          - Usage: 
+            ```
+            web3bms command1 [options]
+            ```
+          - Options:
+            - `--option1`: Description of option 1.
+
+        - **Command 2:** `web3bms command2`
+          - Description: Description of command 2.
+          - Usage: 
+            ```
+            web3bms command2 [options]
+            ```
+          - Options:
+            - `--option2`: Description of option 2.
+        """)
+
+        st.header("PyPI Package")
+        st.write("""
+        You can install the web3bms PyPI package using pip:
+
+        ```
+        pip install web3bms
+        ```
+
+        Once installed, you can import and use the package in your Python scripts.
+
+        Example:
+
+        ```python
+        from web3bms import some_function
+
+        result = some_function(param1, param2)
+        ```
+
+        For more details on how to use the PyPI package, refer to the package documentation on PyPI.
+        """)
+
+    with col2:
+        st.header("API Code Example")
+        st.code("""
+        import requests
+
+        url = "https://your-web3bms-app.com/api/endpoint1?param1=value1"
+        response = requests.get(url)
+        data = response.json()
+        """, language="python")
+
+        st.header("CLI Code Example")
+        st.code("""
+        web3bms command1 --option1 value1
+        """, language="bash")
+
+        st.header("PyPI Code Example")
+        st.code("""
+        from web3bms import some_function
+
+        result = some_function(param1, param2)
+        """, language="python")
 
 page_names_to_funcs = {
     "About ✏️": home_page,
-    # "Meeting Effciency 📈":meetings,
     # "Sign In 🎲": signin,
     "Invoice 📋" : invoice,
-    # "Clients": clients,
-    # "Products": products,
-    # "Opportunties": opportunities,
-    # "API Feeds": api,
+    "Developer Docs 🚝": api,
     "CRM 📪": backend,
     "AI Chat 💻": ai_chat,
     
