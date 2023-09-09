a2df3d2f3f26b5df534d114ce362707dbf6a3e41
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-09-09 13:50:26 -0400

    test spacing

--- app.py
+++ app.py
@@ -97,28 +97,29 @@ st.experimental_set_query_params(page=selected_page_key)
 
 def home_page():
     page_names = ["home", "invoice", "dev_docs", "backend", "ai_chat", "development_request", "ml_ops"]
-    page_labels = ["🏠 Home", "📋 Invoice","📪 CRM", "💻 AI Chat", "🚝 Developer Docs", "☎️ Development Request", "👾 ML Ops"]
-    
+    page_labels = ["🏠 Home", "📋 Invoice", "🚝 Developer Docs", "📪 CRM", "💻 AI Chat", "☎️ Development Request", "👾 ML Ops"]
+
     # Create a horizontal navigation bar
     st.write('<style>div.Widget.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
     selection = st.radio("Navigation", page_labels)
-    
+
     # Define URLs for the pages
     page_urls = {
         "🏠 Home": "https://web3bms.streamlit.app/?page=home",
         "📋 Invoice": "https://web3bms.streamlit.app/?page=invoice",
-        "🚝 Developer Docs": "https://web3bms.streamlit.app/?page=dev_docs",
         "📪 CRM": "https://web3bms.streamlit.app/?page=backend",
         "💻 AI Chat": "https://web3bms.streamlit.app/?page=ai_chat",
+        "🚝 Developer Docs": "https://web3bms.streamlit.app/?page=dev_docs",
         "☎️ Development Request": "https://web3bms.streamlit.app/?page=development_request",
         "👾 ML Ops": "https://web3bms.streamlit.app/?page=ml_ops"
     }
-    
-    # Redirect to the selected page URL
-    if selection in page_urls:
-        url = page_urls[selection]
-        button_html = f'<a href="{url}" target="_self"><button style="background-color: black; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;">{selection}</button></a>'
-        st.markdown(button_html, unsafe_allow_html=True)
+
+    # Loop through page names and labels to create buttons
+    for name, label in zip(page_names, page_labels):
+        url = page_urls.get(label, "")
+        if url:
+            button_html = f'<a href="{url}" target="_self"><button style="background-color: black; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;">{label}</button></a>'
+            st.markdown(button_html, unsafe_allow_html=True)
 
     st.markdown("""
     <script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
