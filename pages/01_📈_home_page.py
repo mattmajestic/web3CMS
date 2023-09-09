ce4e5bab5881f6def060adc6600a4b949b8fc564
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-09-08 20:40:18 -0400

    add page

---
+++ pages/01_📈_home_page.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from streamlit.components.v1 import iframe, html
+import streamlit.components.v1 as components
+from streamlit_javascript import st_javascript
+from wallet_connect import wallet_connect
+import pdfkit
+from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
+from datetime import datetime, date, time, timezone, timedelta
+import pandas as pd
+import requests
+import yfinance as yf
+from pycoingecko import CoinGeckoAPI
+from web3 import Web3, HTTPProvider 
+import json
+import time
+import supabase
+import os
+import folium
+from geopy.geocoders import Nominatim
+import plotly.express as px
+from streamlit import session_state
+import random
+import string
+
+# Read the contents of the README.md file
+with open('../README.md', 'r') as file:
+    readme_text = file.read()
+
+def home_page():
+    st.markdown("""
+    <script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
+    <script>mermaid.initialize({startOnLoad:true});</script>
+    """, unsafe_allow_html=True)
+    st.markdown(readme_text, unsafe_allow_html=True)
