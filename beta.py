bb5a0c5486b49c15232ed70fb143cc88a650addb
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-09-08 23:05:50 -0400

    test 3

--- beta.py
+++ beta.py
@@ -98,7 +98,7 @@ def home_page():
     page_labels = ["🏠 Home", "📋 Invoice", "🚝 Developer Docs", "📪 CRM", "💻 AI Chat"]
     columns = st.columns([2,2,3,2,2])
     for name, label, column in zip(page_names, page_labels, columns):
-        if column.button("Open URL"):
+        if column.button("Open URL", key="open_url_button"):
             url = "https://web3bms.streamlit.app/?page=backend"
             js_code = f"window.location.href = '{url}';"
             st.write(f'<script>{js_code}</script>', unsafe_allow_html=True)
