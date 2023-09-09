c4153c1b8bbe725941c50144c7912e573c6bfa55
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-09-08 22:16:57 -0400

    st up

--- beta.py
+++ beta.py
@@ -215,7 +215,7 @@ def ai_chat():
         response = supabase_client.table("ai-chat").insert([{"prompt": prompt, "created_at": datetime.now().isoformat()}]).execute()
         st.toast('Stored Prompt', icon='✅')
     # Show the BTC Pay Server
-    prompt_expander = right.expander("📝 Show Previous Prompts")
+    prompt_expander = st.expander("📝 Show Previous Prompts")
     with prompt_expander:
         ai_chat_db = supabase_client.table('ai-chat').select("*").execute()
         ai_chat_df = pd.DataFrame(ai_chat_db.data)
