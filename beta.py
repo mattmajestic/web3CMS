e8bc52543e48ac42d7c417a699e22a4c00cfcbc2
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-09-08 22:59:15 -0400

    typo

--- beta.py
+++ beta.py
@@ -98,9 +98,9 @@ def home_page():
     page_labels = ["🏠 Home", "📋 Invoice", "🚝 Developer Docs", "📪 CRM", "💻 AI Chat"]
     columns = st.columns([2,2,3,2,2])
     url = f"https://web3bms.streamlit.app/?page={name}&query={page_queries[name]}"
-        if column.button(label):
-            link = f'<a href="{url}" target="_self">{label}</a>'
-            st.markdown(link, unsafe_allow_html=True)
+    if column.button(label):
+        link = f'<a href="{url}" target="_self">{label}</a>'
+        st.markdown(link, unsafe_allow_html=True)
 
     st.markdown("""
     <script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
