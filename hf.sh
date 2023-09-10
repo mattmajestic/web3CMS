6941c0e75971ea4d15b62d5d5a0bd7760aa830dd
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-09-10 19:06:27 -0400

    Delete hf.sh

--- hf.sh
+++
@@ -1,4 +0,0 @@
-git remote add space https://mattmajestic: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@huggingface.co/spaces/mattmajestic/web3bms
-git add .
-git commit -m "Initial commit"
-git push space main
