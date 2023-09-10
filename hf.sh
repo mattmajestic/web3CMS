b80b35e393f49da4d9f541a593f2f32a2b000859
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-09-10 22:55:06 +0000

    Initial commit

---
+++ hf.sh
@@ -0,0 +1,4 @@
+git remote add space https://mattmajestic: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@huggingface.co/spaces/mattmajestic/web3bms
+git add .
+git commit -m "Initial commit"
+git push space main
