896abfdaefc8b9bf17656ac6dc64aef1952802da
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-12-12 20:15:38 -0500

    revert

--- app/src/components/Settings.js
+++ app/src/components/Settings.js
@@ -5,16 +5,21 @@ function Settings({ session }) {
     const [account, setAccount] = useState('');
 
     useEffect(() => {
-        if (window.ethereum) {
-            loadWeb3();
-        } else {
-            alert('Ethereum object not found. You should consider trying MetaMask!');
-        }
+        window.snowStorm.snowColor = '#99ccff'; // blue snow
+        window.snowStorm.flakesMaxActive = 20;  // show more snowflakes
+        window.snowStorm.useTwinkleEffect = true; // let the snow twinkle
+        window.snowStorm.animationInterval = 10; // 30 FPS
+        window.snowStorm.stop();
     }, []);
 
     async function loadWeb3() {
-        window.ethereum.request({ method: 'eth_requestAccounts' });
-        loadBlockchainData();
+        if (window.ethereum) {
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+            loadBlockchainData();
+        }
+        else {
+            window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+        }
     }
 
     async function loadBlockchainData() {
@@ -22,18 +27,10 @@ function Settings({ session }) {
         setAccount(accounts[0]);
     }
 
-    let sessionObject;
-    try {
-        sessionObject = JSON.parse(JSON.stringify(session));
-    } catch (error) {
-        console.error('Failed to convert session to a JSON object:', error);
-    }
-
     return (
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', fontSize: '20px' }}>
             <h2>Settings</h2>
-            <p>Supabase Session Data:</p>
-            <ReactJson src={sessionObject || {}} theme="chalk" />
+            <p>Supabase Session Data: {JSON.stringify(session)}</p>
             <p>Connected MetaMask Account: {account}</p>
             <button onClick={loadWeb3}>
                 <img src="/mm_logo.png" alt="MetaMask Logo" style={{ width: '20px', height: '20px' }} />
