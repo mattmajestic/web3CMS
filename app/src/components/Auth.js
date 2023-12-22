815254a8be7a1e77151c8989ac6167f70def94f3
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-12-21 19:57:02 -0500

    try linkedin

--- app/src/components/Auth.js
+++ app/src/components/Auth.js
@@ -29,7 +29,7 @@ function SignUp() {
                 <Auth
                     supabaseClient={supabase}
                     appearance={{ theme: ThemeSupa }}
-                    providers={['github', 'bitbucket', 'gitlab', 'google','linkedin_oidc']}
+                    providers={['github', 'bitbucket', 'gitlab', 'google','linkedin']}
                 />
             </div>
         </div>
